ee6cbbb889ed29e30eb26cd09f7821c7811c6897
mozes_njoroge <njorogemosesngure@gmail.com>
2022-07-01 23:36:38 +0300

    Splash screen complete

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -13,44 +13,79 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          // decoration: const BoxDecoration(
-          //   image: DecorationImage(
-          //     image: AssetImage('assets/splash_screen.png'),
-          //   ),
-          // ),
+      body: Container(
+        constraints:
+            BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              colorFilter:
+                  ColorFilter.mode(Colors.grey.shade700, BlendMode.darken),
+              image: const AssetImage('assets/splash_screen.png'),
+              opacity: 0.3,
+              fit: BoxFit.fitHeight),
+        ),
+        child: Flexible(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
             children: [
-              const Text('EUCOSSA'),
-              const FlutterLogo(
-                size: 200,
-                textColor: AppTheme.gradientColor,
-              ),
-              TextButton(
-                onPressed: () {
-                  //todo: open registration page
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Register()),
-                  );
-                },
-                child: const Text(
-                  'GET STARTED',
-                  style: TextStyle(
-                    color: AppTheme.gradientColor,
-                  ),
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: const [
+                    Text(
+                      'EUCOSSA',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Color(0xFFFF9800),
+                      ),
+                    ),
+                    FlutterLogo(
+                      size: 350,
+                      textColor: AppTheme.gradientColor,
+                    ),
+                  ],
                 ),
               ),
-              const Text('By Pressing \'Get started\' you agree to our'),
-              const Text(
-                'privacy polcy & terms of service',
-                style: TextStyle(
-                  color: AppTheme.gradientColor,
+              Flexible(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange.shade700),
+                      ),
+                      onPressed: () {
+                        //todo: open registration page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Register()),
+                        );
+                      },
+                      child: const Text(
+                        'GET STARTED',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'By Pressing \'Get started\' you agree to our',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                    const Text(
+                      'privacy policy & terms of service',
+                      style: TextStyle(
+                          color: AppTheme.gradientColor, fontSize: 15),
+                    )
+                  ],
                 ),
-              )
+              ),
             ],
           ),
         ),
--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
 
-class AppTheme{
-  static const Color primaryColor=Color(0xff114f5a);
-  static const Color gradientColor=Color(0xffc49450);
-  static const Color whiteColor=Color(0xffffffff);
-  static const Color lightColor=Color(0xfff7f8fb);
+class AppTheme {
+  static const Color primaryColor = Color(0xff114f5a);
+  static const Color gradientColor = Color(0xFFFF9800);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color lightColor = Color(0xfff7f8fb);
 }
